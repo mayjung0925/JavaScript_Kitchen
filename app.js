b0c5ea549e2251196ade91ca2d3ee07327b826e4
Eunjin Jeoung <mayjung0925@gmail.com>
2022-01-26 13:10:49 -0600

    JavaScript Kitchen Sink

---
+++ app.js
@@ -0,0 +1,66 @@
+var firstName = 'Eunjin';
+const numberOfStates = 50;
+var add = 5+4
+
+function sayHello(){
+    alert("Hello World!")  
+}
+
+sayHello();
+
+function checkAge(name, age){
+    if( age <21){
+        alert("Sorry"+" "+ name + ",you aren't old enough to view this page!");
+    }
+
+}
+
+checkAge('charles',21);
+checkAge('Abby',27);
+checkAge('James', 18);
+checkAge('John',17);
+
+var vegetables = ['carrots','onions','potatoes','mushrooms','leeks','beets'];
+
+for (let i = 0 ; i<vegetables.length ; i++){
+    console.log(vegetables[i]);
+}
+
+var pet = {
+    name : 'Jenny',
+    breed : 'Goldendoodle'
+
+}
+
+
+console.log(pet);
+var customers = [
+
+    {name : 'Sam', age: 15},
+    {name : 'Ann', age : 20},
+    {name:'Maggie', age : 4},
+    {name:'Sally', age : 35},
+    {name:'Carol', age : 60}
+];
+
+for ( let i = 0 ; i <customers.length ; i ++){
+    checkAge(customers[i].name , customers[i].age);
+}
+
+
+function getLength(text) {
+    let  length = text.length;
+ 
+    if ( length % 2 === 0){
+        console.log('The world is nice and even!')
+    } else console.log ( 'The world is an odd place!')
+ 
+}
+
+ getLength('Hello World');
+ 
+
+
+
+
+
